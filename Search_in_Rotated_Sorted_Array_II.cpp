85b4b286ee8be7f0f5fa373fc2d7b05b61251c7a
guolizihaohao <guolizihao@hotmail.com>
2015-08-02 20:25:44 +0800

    Create Search_in_Rotated_Sorted_Array_II.cpp

---
+++ Search_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } 
+            if (nums[mid] > nums[low]) {  //转折点在右边部分，这样左边部分是升序排列的
+                if (nums[low] <= target && target < nums[mid]) { //满足这个条件就进入了没有转折点、全部都是升序的序列
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] < nums[low]) { //这里表示转折点在左边或者已经没有转折点了，既已经进入了完全升序的序列
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low++;
+            }
+        }
+        return false;
+    }
+};
