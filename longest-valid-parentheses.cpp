fbaafff0039756a9fe00b313d69900ddb65df2d3
guolizihaohao <guolizihao@hotmail.com>
2015-08-05 11:24:26 +0800

    Update longest-valid-parentheses.cpp

--- longest-valid-parentheses.cpp
+++ longest-valid-parentheses.cpp
@@ -6,6 +6,7 @@ public:
         if (s.size() == 0) return 0;
         stack<int> stk;
         int start = 0, res = 0;
+        // start 表示当前不符合要求的'('开始的位置
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(') {
                 stk.push(i);
@@ -16,8 +17,9 @@ public:
                 } else {
                     stk.pop();
                     res = stk.empty() ? max(res, i - start + 1) : max(res, i - stk.top()); 
-                    //i - stk.top() = i - (stk.top() + 1) + 1 
-                    // stk.top() + 1 表示当前匹配连续字符串最开始的位置
+                    // i - stk.top() = i - (stk.top() + 1) + 1 
+                    // stk.top() 还在stack中，说明它不符合要求
+                    // 那么stk.top() + 1 表示当前符合要求的连续字符串开始的位置
                 }
             }
         }
