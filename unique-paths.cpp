b78d6a442ae4cfe03910a6b587c3175d8da67449
guolizihaohao <guolizihao@hotmail.com>
2015-08-07 14:22:32 +0800

    Update unique-paths.cpp

--- unique-paths.cpp
+++ unique-paths.cpp
@@ -12,3 +12,18 @@ public:
         return dp[n - 1];
     }
 };
+
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         double dom = 1;
+//         double dedom = 1;
+//         int small = m<n? m-1:n-1;
+//         int big = m<n? n-1:m-1;
+//         for(int i=1;i<=small;i++) {
+//             dedom *= i;
+//             dom *= small+big+1-i;
+//         }
+//         return (int)(dom/dedom);
+//     }
+// };
