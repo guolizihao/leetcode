6f21e15dc26d6b38ae4b455be24553d4e7a4055d
guolizihaohao <guolizihao@hotmail.com>
2015-08-07 14:51:53 +0800

    Create unique-paths-ii.cpp

---
+++ unique-paths-ii.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/unique-paths-ii/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        vector<int> dp(m, 0);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                else if (j > 0) dp[j] += dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+};
