1fc18c087178fa69457f55f3363feb2257a21635
guolizihaohao <guolizihao@hotmail.com>
2015-08-07 12:34:34 +0800

    Create unique-paths.cpp

---
+++ unique-paths.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/unique-paths/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);  //第0行每个位置的走法都为1
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+};
