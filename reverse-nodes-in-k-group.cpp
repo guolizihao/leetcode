d4106fa06a5bfafc6484ef05616e0c1f8a741d21
guolizihaohao <guolizihao@hotmail.com>
2015-08-21 08:07:51 +0800

    Create reverse-nodes-in-k-group.cpp

---
+++ reverse-nodes-in-k-group.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *pre = dummy, *cur = head;
+        dummy->next = head;
+        int i = 0;
+        while (cur) {
+            ++i;
+            if (i % k == 0) {
+                pre = reverse(pre, cur->next);
+                cur = pre->next;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return dummy->next;
+    }
+    
+    ListNode* reverse(ListNode *pre, ListNode *next) {
+        ListNode *last = pre->next;
+        ListNode *cur = last->next;
+        while (cur != next) {
+            last->next = cur->next;
+            cur->next = pre->next;
+            pre->next = cur;
+            cur = last->next;
+        }
+        return last;
+    }
+};
