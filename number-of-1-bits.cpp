c2cb5ac86d9d3dbd6014cfd32d181a47dfe17833
guolizihaohao <guolizihao@hotmail.com>
2015-09-01 16:32:45 -0400

    Create number-of-1-bits.cpp

---
+++ number-of-1-bits.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        // while (n) {
+        //     res += n & 1;
+        //     n >>= 1;
+        // }
+        while (n) {
+            ++res;
+            n &= n - 1;
+        }
+        return res;
+    }
+};
