1aed5524a2c904dbfb37cb2051ea3dceca550c89
guolizihaohao <guolizihao@hotmail.com>
2015-09-03 15:49:49 -0400

    Update binary-tree-inorder-traversal.cpp

--- binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal.cpp
@@ -9,25 +9,54 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
         if (!root) return res;
-        TreeNode *node = root;
-        stack<TreeNode *> s;
-        while (!s.empty() || node) {
-            while (node) {
-                s.push(node);
-                node = node->left;
+        TreeNode *cur, *pre;
+        cur = root;
+        while (cur) {
+            if (!cur->left) {
+                res.push_back(cur->val);
+                cur = cur->right; // insert the left and right children
+            } else {
+                pre = cur->left;
+                while (pre->right && pre->right != cur) pre = pre->right;
+                if (!pre->right) {
+                    pre->right = cur;
+                    cur = cur->left;
+                } else {
+                    pre->right = NULL;
+                    res.push_back(cur->val);
+                    cur = cur->right; // insert the parents
+                }
             }
-            node = s.top(); s.pop();
-            res.push_back(node->val);
-            node = node->right;
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         if (!root) return res;
+//         TreeNode *node = root;
+//         stack<TreeNode *> s;
+//         while (!s.empty() || node) {
+//             while (node) {
+//                 s.push(node);
+//                 node = node->left;
+//             }
+//             node = s.top(); s.pop();
+//             res.push_back(node->val);
+//             node = node->right;
+//         }
+//         return res;
+//     }
+// };
 
 // class Solution {
 // public:
