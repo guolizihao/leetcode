4d6f7acf3387075cc3a1f3ac2f7367171312bfd8
guolizihaohao <guolizihaohao@gmail.com>
2015-12-28 17:44:02 -0500

    Create move-zeroes.cpp

---
+++ move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        if (nums.size() <= 1) return;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.size(); ++j) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+};
